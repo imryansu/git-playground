800d3afa758f11490714e8ef9f59e956b3183957
Ryan Su <imryansu@icloud.com>
2025-10-24 12:17:00 +0800

    feat: add configuration module (#1)
    * WIP: add config file
    * add api config
    * add more config options
    * docs: update config documentation

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * Configuration module
+ * Manages application-wide configuration settings
+ */
+
+const config = {
+  apiUrl: 'https://api.example.com',
+  timeout: 5000,
+  retries: 3,
+  debug: false,
+};
+
+module.exports = config;
